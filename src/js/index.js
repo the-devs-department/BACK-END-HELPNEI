4437415d57e0ea13cbfc4e93b2897cb684f5d50e
tiagow2 <tiago.bala02@gmail.com>
2025-03-24 16:25:45 -0300

    Realizar requisições do front-end para o back-end

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '..', '..', 'FRONT-END-HELPNEI', 'dist')));
+
 app.get('/', (req, res) => {
-  res.send('Aqui será a pagina do projeto');
+    res.sendFile(path.join(__dirname, '..', '..', 'FRONT-END-HELPNEI', 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`O link do projeto: http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
